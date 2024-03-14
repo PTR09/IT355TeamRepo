bd13ffb3b5e2a14d65cdce8947c3ccab38f04c49
ChillJacob <41339964+ChillJacob@users.noreply.github.com>
2024-03-14 19:28:10 -0400

    Add files via upload

---
+++ CodeExamples/OBJ10.java
@@ -0,0 +1,12 @@
+
+public class OBJ10
+{
+    //private final static variable
+    private final static String helloWorld = "Hello World!";
+    public static void main(String[] args)
+    {
+        //prints hello world using the private final variable practice
+        System.out.println(helloWorld);
+    }//end main
+
+}//end class

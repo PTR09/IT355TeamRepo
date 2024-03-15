883d3ecdfa5ae35c12f68a9023c12097ddd50bb2
ChillJacob <41339964+ChillJacob@users.noreply.github.com>
2024-03-14 21:48:17 -0400

    Add files via upload

---
+++ CodeExamples/MET05.java
@@ -0,0 +1,13 @@
+public class MET05
+{
+    public static void main(String[] args)
+    {
+        sayHello();
+    }//end main
+
+    //declared as final so cannot be overridden
+    public static final void sayHello()
+    {
+        System.out.println("Hello!");
+    }
+}//end class

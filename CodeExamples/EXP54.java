4c35f299014e8be1fa2949e9690195a1e3f567ff
PTR09 <71910300+PTR09@users.noreply.github.com>
2024-03-15 16:03:47 -0500

    Added EXP54

---
+++ CodeExamples/EXP54.java
@@ -0,0 +1,15 @@
+public class EXP54 {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+        boolean z = (x == y);
+
+        int a = 1;
+        int b = 1;
+        int c = a & b;
+        
+        System.out.println(z);
+        System.out.println(c);
+    }
+}

a77f5c16aa601d6853163e9d44e413e09be09911
PTR09 <71910300+PTR09@users.noreply.github.com>
2024-03-15 15:50:55 -0500

    Added EXP51

---
+++ CodeExamples/EXP51.java
@@ -0,0 +1,9 @@
+public class EXP51 {
+
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x == 5) // Proper use of conditional expression, incorrect would be single =
+            System.out.println("x is 5");
+    }
+}

a1d869e0ff21a2a6cfff5124f2596b3bce05ba72
Chloe Todd <78936274+EttyYeti@users.noreply.github.com>
2024-03-15 15:07:20 -0500

    Add files via upload

---
+++ CodeExamples/DCL50.java
@@ -0,0 +1,9 @@
+public class DCL50 {
+    public static void main(String[] args){
+        // Do not do this:
+        //System.out.println(1111 + 111l);
+
+        System.out.println(1111 + 1111L);
+    }
+    
+}

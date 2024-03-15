e623ae433830adaceafb5103b2e4ef46f0f62dca
PTR09 <71910300+PTR09@users.noreply.github.com>
2024-03-15 15:54:55 -0500

    Added EXP52

---
+++ CodeExamples/EXP52.java
@@ -0,0 +1,11 @@
+public class EXP52 {
+
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x == 5)
+        {
+            System.out.println("x is 5");
+        }
+    }
+}

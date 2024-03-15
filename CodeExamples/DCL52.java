cd87a0ea171c5c6f94ff7468a0427dbc8c8260dd
Chloe Todd <78936274+EttyYeti@users.noreply.github.com>
2024-03-15 14:51:00 -0500

    Add files via upload

---
+++ CodeExamples/DCL52.java
@@ -0,0 +1,12 @@
+public class DCL52 {
+    public static void main(String[] args){
+        // Do not do this:
+        // int a, b = 1;
+
+        int a = 1;
+        int b = 1;
+
+        System.out.println(a + b);
+    }
+    
+}

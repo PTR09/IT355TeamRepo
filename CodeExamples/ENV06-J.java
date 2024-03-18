0e57bf4778a78d77b8ea353471cfc0d7bc105bf3
mebaize <160536552+mebaize@users.noreply.github.com>
2024-03-17 23:06:04 -0500

    Update ENV06-J.java

---
+++ CodeExamples/ENV06-J.java
@@ -0,0 +1,8 @@
+class anyClass {
+  //in the public release of this, we would want
+  //to ensure DEBUG is false.
+  final static boolean DEBUG = false;
+  public void anyCode(){
+    //enter any code. 
+  }
+}
